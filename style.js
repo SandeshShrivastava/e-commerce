7d450b29223a73efb61fb64bcffee9154d8c1e7e
Tinu Shrivastava <127187470+SandeshShrivastava@users.noreply.github.com>
2023-09-25 19:39:52 +0530

    Add files via upload

--- style.js
+++ style.js
@@ -1,1 +1,11 @@
-
+function btn() {
+  var inp = document.getElementById("inp").value;
+  var p = document.createElement("p");
+  p.innerText = inp;
+  console.log(inp); 
+}
+function chook() {
+  Colors = ["white", "grey" ];
+  sel = +document.getElementById("okok").value;
+  document.body.style = "background-color:" + Colors[sel];
+}
